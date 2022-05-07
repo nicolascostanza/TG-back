6a6a9c56122a8d81c839b4fc2ffc860ad7f7eb7c
maria higinia medica <higiniamc@gmail.com>
2022-05-07 19:11:06 -0300

    BaSP-W08-2022: Add routers

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@
 import express from 'express';
 
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
+const projects = require('./data/projects.json');
+const projectsRouter = require('./resources/projects');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/projects', projectsRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/admins', (req, res) => {
+app.get('/projects', (req, res) => {
   res.status(200).json({
-    data: admins,
+    data: projects,
   });
 });
 
@@ -23,4 +25,3 @@ app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
-// new branch
