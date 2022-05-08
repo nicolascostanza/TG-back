a1f1b44023a9515ee9c1962b36ca76d51e59538d
Sebastián Ronchetti <ronchettisebastian@gmail.com>
2022-05-08 20:14:14 -0300

    BaSP-W08-2022: Delete, post, put and filter by id options added

--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -1,26 +1,23 @@
 import express from 'express';
 const res = require("express/lib/response");
+const fileSystem = require('fs');
 const router = express.Router();
+const jAdress = 'src/data/tasks.json';
 
 // use "require" to import JSON files
 const tasks = require('../data/tasks.json');
 
-router.get('/getAllTasks', (req, res) => {
-    res.status(200).json({
-        data: tasks,
-    });
+router.get('/taskList', (req, res) => {
+    res.status(200).json({data: tasks});
 });
   
-router.get('/getTaskById/:id', (req, res) => {
+router.get('/findById/:id', (req, res) => {
     const taskId = req.params.id;
-    const task = tasks.find((task) =>{
-        return task.id === taskId
-    });
+    const task = tasks.find((task) =>{return task.id === taskId});
+
     if(task)
     {
-        res.status(200).json({
-            data : task
-        });
+        res.status(200).json({data : task});
     }
     else
     {
@@ -28,31 +25,110 @@ router.get('/getTaskById/:id', (req, res) => {
     }
 });
 
-router.get('/getTasksByStatus', (req, res) => {
+router.get('/filterByStatus', (req, res) => {
     const taskStatus = req.query.status;
-    const compliantTasks = tasks.filter((task) =>{
-        return task.status === taskStatus
-    });
+    const compliantTasks = tasks.filter((task) => {return task.status === taskStatus});
+
     if(compliantTasks.length > 0)
     {
-        res.status(200).json({
-            data : compliantTasks
-        });
+        res.status(200).json({data : compliantTasks});
     }
     else
     {
         res.status(404).send('The specified task does not exist');
     }
+});
+
+router.post('/add', (req, res) => {
+    const taskData = json(req.body);
+    const taskId = req.params.id;
+    const comparator = tasks.find((task) => {return task.id === taskId});
+
+    if(comparator)
+    {
+        res.status(409).send('Requested id already exist on database');
+    }
+    else
+    {
+        if(taskData.parentProject && taskData.id && taskData.taskCreator && taskData.taskName
+            && taskData.startDate)
+        {
+            tasks.push(taskData);
+            fileSystem.writeFile(jAdress, JSON.stringify(tasks), (err) => {
+                if(err)
+                {
+                    res.status(500).send(err);
+                }
+                else
+                {
+                    res.status(201).send('Task has been successfully created');
+                }
+            })
+        }
+        else
+        {
+            res.status(400).send('One or more Key fields are not valid');
+        }
+    }
 })
 
-router.post('/', (req, res) => {
-    res.send(req.body)
+router.put('/:id', (req, res) => {
+    const taskId = req.params.id;
+    const comparator = tasks.find((task) => {return task.id === taskId});
+    const taskChanges = req.body;
+
+    if(comparator)
+    {
+        tasks.forEach(task => {
+            if(task.id === taskId)
+            {
+                task.Name = taskChanges.Name ? taskChanges.Name : task.Name
+                task.Description = taskChanges.Description ? taskChanges.Description : task.Description;
+                task.assignedEmployee.employeeId = taskChanges.assignedEmployee.employeeId ? taskChanges.assignedEmployee.employeeId : task.assignedEmployee.employeeId;
+                task.assignedEmployee.employeeRole = taskChanges.assignedEmployee.employeeRole ? taskChanges.assignedEmployee.employeeRole : task.assignedEmployee.employeeRole;
+                task.assignedEmployee.employeeName = taskChanges.assignedEmployee.employeeName ? taskChanges.assignedEmployee.employeeName : task.assignedEmployee.employeeName;
+                task.status = taskChanges.status ? taskChanges.status : task.status;
+            }
+        })
+
+        fileSystem.writeFile(jAdress, JSON.stringify(tasks), (err) => {
+            if(err)
+            {
+                res.status(500).send(err);
+            }
+            else
+            {
+                res.status(200).send('Task has been successfully edited');
+            }
+        })
+    }
+    else
+    {
+        res.status(409).send('Requested id does not exist on database');
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    res.status(200).json({
-        data: tasks.filter(task => task.id == req.params.id)
-    })
+router.delete('/delete/:id', (req, res) => {
+    const taskId = req.params.id;
+    const filteredTasks = tasks.filter((task) => {return task.id !== taskId});
+
+    if(filteredTasks.length < tasks.length)
+    {
+        fileSystem.writeFile(jAdress, JSON.stringify(filteredTasks), (err) => {
+            if(err)
+            {
+                res.status(500).send(err, 'Holatrolo');
+            }
+            else
+            {
+                res.status(200).send('Task has been successfully deleted');
+            }
+        })
+    }
+    else
+    {
+        res.status(409).send('Requested id does not exist on database');
+    }
 });
 
 module.exports = router
