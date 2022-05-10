f7998a2b64327523eb76dba22ee755df41773546
Cocci Sebastian <sebacocci@gmail.com>
2022-05-10 16:30:37 -0300

    TG:finished-post

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 // use "import" to import libraries
+
 import express from 'express';
-import * as employeesControllers from'./resources/employees';
 
 // use "require" to import JSON files
-const employees = require('./data/employees.json');
+
+const employees = require('./resources/employees');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/employees', employees);
 
 app.get('/', async (req, res) => {
-  res.send('Hello Alva!');
-});
-
-app.get('/emplyees', (req, res) => {
-  res.status(200).json({
-    data: employees,
-  });
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
 });
-
-app.get('/employee/:id', employeesControllers.getEmployeeById)
--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,22 +1,55 @@
+const express = require('express');
+
+const fs = require('fs');
+
+const router = express.Router();
 const employees = require('../data/employees.json');
-/* const express = require('express'); */
 
+router.get('/getAll', (req, res) => {
+  res.status(200).json({
+    employees,
+  });
+});
 
-const getEmployeeById = async (req, res) => {
-    const id = req.params.id;
-    const employeeFind = employees.find(item => item.id == parseInt(id));
-/* 
-    console.log('Employee', employeeFind) */
+router.get('/getById/:id', (req, res) => {
+  const employeesId = req.params.id;
+  const users = employees.find((employee) => employee.id === employeesId);
+  if (users) {
+    res.send(users);
+  } else {
+    res.send('user not found');
+  }
+});
 
-    if (!employeeFind) {
-        res.json({
-            msg: `This employee with ID ${id} does not exist`
-        })
-    } res.json({
-        data: employeeFind
-    })
+router.get('/getByFname/', (req, res) => {
+  const employeesFname = req.query.full_name;
+  const Fname = employees.filter((name) => name.full_name === employeesFname);
+  if (Fname.length > 0) {
+    res.send(Fname);
+  } else {
+    res.send(`${employees.Fname} not found`);
+  }
+});
 
-}
-export {
-    getEmployeeById
-}
+router.post('/add', (req, res) => {
+  const userData = req.body;
+  if (userData.id && userData.full_name && userData.email && userData.gender && userData.address
+    && userData.dob && userData.password && userData.phone && userData.active !== '') {
+    if (employees.every((employee) => userData.id !== employee.id)) {
+      employees.push(userData);
+      fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send('User created');
+        }
+      });
+    } else {
+      res.send('This id already exists');
+    }
+  } else {
+    res.send('This user is incomplete');
+  }
+});
+
+module.exports = router;
