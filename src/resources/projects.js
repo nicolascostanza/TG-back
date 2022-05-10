fdbeb4f426a38428a685442cba2511f3a3055255
maria higinia medica <higiniamc@gmail.com>
2022-05-10 00:42:14 -0300

    BaSP-W08-2022: Add filter code

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -10,7 +10,7 @@ const projects = require('../data/projects.json');
 
 router.delete('/delete/:id', (req, res) => {
   const projectId = req.params.id;
-  const filteredProjects = projects.filter((project) => project.id !== projectId);
+  const filteredProjects = projects.filter((project) => project.id.toString() !== projectId);
   if (projects.length === filteredProjects.length) {
     res.send(`No project with the id of ${req.params.id}`);
   } else {
@@ -28,13 +28,13 @@ router.delete('/delete/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
   const projectId = req.params.id;
-  const filteredProjects = projects.find((project) => project.id === projectId);
+  const filteredProjects = projects.find((project) => project.id.toString() === projectId);
   const updProject = req.body;
 
   if (filteredProjects) {
     projects.forEach((project) => {
       const newProject = project;
-      if (project.id === projectId) {
+      if (project.id.toString() === projectId) {
         newProject.name = updProject.name ? updProject.name : project.name;
         newProject.description = updProject.description ? updProject.description
           : project.description;
@@ -61,5 +61,52 @@ router.put('/:id', (req, res) => {
 });
 
 // Filter list
+// By name
+
+router.get('/getByName', (req, res) => {
+  const projectName = req.query.name;
+  const filteredProjects = projects.filter((project) => project.name.toString() === projectName);
+  if (filteredProjects.length > 0) {
+    res.send(filteredProjects);
+  } else {
+    res.send(`No project with the id of ${req.params.id}`);
+  }
+});
+
+// By client name
+
+router.get('/getByClientName', (req, res) => {
+  const projectClient = req.query.clientName;
+  const filtProject = projects.filter((project) => project.clientName.toString() === projectClient);
+  if (filtProject.length > 0) {
+    res.send(filtProject);
+  } else {
+    res.send(`No project with the id of ${req.params.id}`);
+  }
+});
+
+// By start date
+
+router.get('/getByStartDate', (req, res) => {
+  const projectStDate = req.query.startDate;
+  const filtProject = projects.filter((project) => project.startDate.toString() === projectStDate);
+  if (filtProject.length > 0) {
+    res.send(filtProject);
+  } else {
+    res.send(`No project with the id of ${req.params.id}`);
+  }
+});
+
+// By status
+
+router.get('/getByActive', (req, res) => {
+  const projectActive = req.query.active;
+  const filtProject = projects.filter((project) => project.active.toString() === projectActive);
+  if (filtProject.length > 0) {
+    res.send(filtProject);
+  } else {
+    res.send(`No project with the id of ${req.params.id}`);
+  }
+});
 
 module.exports = router;
