0b0114cf044a9bb82f0844b7f203d1b1529dd3e8
vadalamati <101281359+vadalamati@users.noreply.github.com>
2022-05-14 17:24:28 -0300

    TG-23 corrected an error in routes/tasks.js

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -4,6 +4,6 @@ import tasksControllers from '../controllers/tasks';
 const router = express.Router();
 
 router.get('/', tasksControllers.getAllTasks);
-router.get('/id', tasksControllers.getTasksById);
+router.get('/:id', tasksControllers.getTasksById);
 
 export default router;
