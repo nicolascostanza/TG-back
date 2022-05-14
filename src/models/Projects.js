1cbf184abbae34f307b37762a84b0c62f740d551
Juan Moreira <juanmoreira1518@gmail.com>
2022-05-14 19:02:31 -0300

    review comments solved

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -31,10 +31,18 @@ const projectSchema = new Schema(
     },
     team: [
       {
+        id: { type: String, required: true },
         name: { type: String, required: true },
-        role: { type: String, required: true },
+        role: { type: String, enum: ['DEV', 'QA', 'PM', 'TL'], required: true },
         hours: { type: Number, required: true },
         rate: { type: Number, required: true },
+      },
+    ],
+    tasks: [
+      {
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+        descrption: { type: String, required: true },
       },
     ],
     adminId: {
