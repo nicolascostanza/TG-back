4d289011ab7d6ba9257a85c05650d6c5e9c4630c
Juan Moreira <juanmoreira1518@gmail.com>
2022-05-13 22:30:44 -0300

    TG-28: Project model created

---
+++ src/models/Projects.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const projectSchema = new Schema(
+
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    clientName: {
+      type: String,
+      required: true,
+    },
+
+    startDate: {
+      type: Date,
+      required: true,
+    },
+
+    endDate: {
+      type: Date,
+      required: false,
+    },
+
+    projectManager: {
+      type: String,
+      required: true,
+    },
+
+    team: [
+      {
+        name: { type: String, required: true },
+        role: { type: String, required: true },
+        hours: { type: Number, required: true },
+        rate: { type: Number, required: true },
+      },
+    ],
+
+    /* adminId: {
+      type: Number,
+      required: true,
+    }, */
+
+  },
+);
+
+export default mongoose.model('Project', projectSchema);
