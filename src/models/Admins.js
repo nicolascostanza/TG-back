9e128c6feb0fdc7343bfbbd186ea52fc759e7080
maria higinia medica <higiniamc@gmail.com>
2022-05-13 22:30:51 -0300

    TG-29: Admin model

---
+++ src/models/Admins.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const adminSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+);
+
+export default mongoose.model('Admin', adminSchema);
