b139bc0f7b4233638d768a2d7dee8207bb1ded71
Sebastián Ronchetti <ronchettisebastian@gmail.com>
2022-05-14 16:08:29 -0300

    TG-16: Renamed model properties and fixed minor errors

---
+++ src/models/employee.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const employeeSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    surname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    gender: {
+      type: String,
+      required: false,
+    },
+    adress: {
+      type: String,
+      required: false,
+    },
+    dob: {
+      type: Date,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 8,
+    },
+    phone: {
+      type: Number,
+      required: false,
+      min: 10,
+    },
+    active: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model('Employee', employeeSchema);
