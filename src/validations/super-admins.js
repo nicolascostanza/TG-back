5e20e75f6f823d94ce33cb35069910744fb50c90
LuliBianchini <lulibianchini97@gmail.com>
2022-05-15 18:45:24 -0300

    TG-36: Controllers, routes and post validation

---
+++ src/validations/super-admins.js
@@ -0,0 +1,25 @@
+import Joi from "joi";
+
+const validateCreation = (req, res, next) => {
+  const superAdminValidation = Joi.object({
+    firstName: Joi.string().min(1).max(50).required(),
+    lastName: Joi.string().min(1).max(50).required(),
+    email: Joi.string().lowercase().required(),
+    password: Joi.string().alphanum().required(),
+    active: Joi.boolean().required(),
+  })
+
+  const validation = superAdminValidation.validate(req.body);
+  if (validation.error) {
+    return res.status(400).json({
+      message: validation.error.details[0].message,
+      data: undefined,
+      error: true
+    });
+  }
+  return next();
+}
+
+export default {
+  validateCreation
+}
