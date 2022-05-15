dc4e40e4cf394478efcca222c7aa12e18cb9f8f5
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-05-15 14:21:22 -0300

    TG-27: Changes-in-names

--- src/controllers/time-sheets.js
+++ src/controllers/time-sheets.js
@@ -19,16 +19,16 @@ const getAllTs = async (req, res) => {
 
 const getTsById = async (req, res) => {
   try {
-    if (req.params.employeeId) {
-      const empId = await models.findById(req.params.employeeId);
+    if (req.params.id) {
+      const empId = await models.findById(req.params.id);
       res.status(200).json({
-        message: `The data fot the employee whit id ${req.params.employeeId} has been sent`,
+        message: `The data fot the employee whit id ${req.id} has been sent`,
         data: empId,
         error: false,
       });
     } else {
       res.status(400).json({
-        message: `There are not employee whit id ${req.params.employeeId}`,
+        message: `There are not employee whit id ${req.params.id}`,
         data: undefined,
         error: true,
       });
