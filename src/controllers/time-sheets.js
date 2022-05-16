b74392c852c3aca1a7929eef2065d228bb8ecec6
Alvaro-Guardia <alvaroguardia95@gmail.com>
2022-05-16 15:24:27 -0300

    TG-27: Changues-in-postman-and-controller

--- src/controllers/time-sheets.js
+++ src/controllers/time-sheets.js
@@ -3,13 +3,20 @@ import models from '../models/Time-sheets';
 const getAllTs = async (req, res) => {
   try {
     const getAllT = await models.find({});
-    res.status(200).json({
+    if (getAllT.length <= 0) {
+      return res.status(400).json({
+        message: ' No Time-sheets f',
+        data: null,
+        error: true,
+      });
+    }
+    return res.status(200).json({
       message: ' Data for all Time-sheets sended',
       data: getAllT,
       error: false,
     });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error,
       data: undefined,
       error: true,
@@ -19,24 +26,28 @@ const getAllTs = async (req, res) => {
 
 const getTsById = async (req, res) => {
   try {
-    if (req.params.id) {
-      const empId = await models.findById(req.params.id);
-      if (empId) {
-        res.status(200).json({
-          message: `The data fot the employee whit id ${req.params.id} has been sent`,
-          data: empId,
-          error: false,
-        });
-      }
-    } else {
-      res.status(400).json({
-        message: `There are not employee whit id ${req.params.id}`,
-        data: undefined,
-        error: true,
+    if (!req.params.id) {
+      return res.status(400).json({
+        message: 'Missing id parameters',
+        data: null,
+        error: false,
       });
     }
+    const empId = await models.findById(req.params.id);
+    if (empId) {
+      return res.status(200).json({
+        message: `The data fot the employee whit id ${req.params.id} has been sent`,
+        data: empId,
+        error: false,
+      });
+    }
+    return res.status(400).json({
+      message: `There are not employee whit id ${req.params.id}`,
+      data: undefined,
+      error: true,
+    });
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       message: error,
       data: undefined,
       error: true,
