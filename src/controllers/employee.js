ee1764a6ce4943495ed9c96c535d42dcf66e5e1c
Sebastián Ronchetti <ronchettisebastian@gmail.com>
2022-05-16 16:25:25 -0300

    TG-18: Delete operational and made 2 more data employees

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -29,21 +29,21 @@ const createEmployee = async (req, res) => {
 
 const deleteEmployee = async (req, res) => {
   try {
-    if (!req.params.id) {
+    if (!req.body.id) {
       res.status(400).json({
         msg: 'missing Id param',
       });
     }
 
-    const result = await Employee.findByIdAndDelete(req.params.id);
+    const result = await Employee.findByIdAndDelete(req.body.id);
 
     if (!result) {
       return res.status(404).json({
-        msg: `The employee with an id of ${req.params.id} has not been found or does not exist`,
+        msg: `The employee with an id of ${req.body.id} has not been found or does not exist`,
       });
     }
     return res.status(200).json({
-      msg: `The employee with former id of ${req.params.id} has been succesfully deleted`,
+      msg: `The employee with former id of ${req.body.id} has been succesfully deleted`,
     });
   } catch (error) {
     return res.json({
