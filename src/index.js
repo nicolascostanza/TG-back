a63b8b63bb134286ccff2420fde5f5ff79ffe55e
Javier Cervera <javieralejandrocervera@gmail.com>
2022-05-16 14:54:35 -0300

    TG-22: Re-add imports and app.use in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
+import timesheets from './controllers/time-sheets';
+import admins from './controllers/admins';
+import projects from './controllers/projects';
+import superAdminsRouter from './controllers/super-admins';
 
 const URL = 'mongodb+srv://AlfonsoDalix:RadiumRocket@basp-tg.amp3e.mongodb.net/BaSP-TG?retryWrites=true&w=majority';
 const app = express();
@@ -8,6 +12,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(router);
+app.use('/superAdmin', superAdminsRouter);
+app.use('/admins', admins);
+app.use('/projects', projects);
+app.use('/time-sheets', timesheets);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
