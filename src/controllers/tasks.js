2cb4b798bf44023834fa613b4569808ba2c49f2f
Matias <vadalamati@gmail.com>
2022-05-17 02:58:57 -0300

    TG-23: added controllers for tasks (getAll and getByID)

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,56 +1,41 @@
 import Task from '../models/Tasks';
 
-const createTask = async (req, res) => {
+const getAllTasks = async (req, res) => {
   try {
-    const task = new Task({
-      parentProject: req.body.parentProject,
-      taskCreatorId: req.body.taskCreatorId,
-      taskName: req.body.taskName,
-      taskDescription: req.body.taskDescription,
-      assignedEmployee: req.body.assignedEmployee,
-      startDate: req.body.startDate,
-      status: req.body.status,
-    });
-    const result = await task.save();
-    return res.status(201).json({
-      message: 'Task has been created',
-      data: result,
+    const allTasks = await Task.find({});
+    res.status(200).json({
+      message: 'Tasks data sended',
+      data: allTasks,
       error: false,
     });
   } catch (error) {
-    return res.status(400).json({
-      message: error,
+    res.status(500).json({
+      message: 'Internal server error',
       data: undefined,
       error: true,
     });
   }
 };
 
-const deleteTask = async (req, res) => {
+const getTasksById = async (req, res) => {
   try {
-    if (!req.params.id) {
-      return res.status(400).json({
-        message: 'Missing id parameter',
+    if (req.params.id) {
+      const task = await Task.findById(req.params.id);
+      res.status(200).json({
+        message: 'Filtered tasks by id has been sent',
+        data: task,
+        error: false,
+      });
+    } else {
+      res.status(400).json({
+        message: `There are not task whit id ${req.params.id}`,
         data: undefined,
         error: true,
       });
     }
-    const result = await Task.findByIdAndDelete(req.params.id);
-    if (!result) {
-      return res.status(404).json({
-        message: `The task with ID: ${req.params.id} hasn't been found`,
-        data: undefined,
-        error: true,
-      });
-    }
-    return res.status(200).json({
-      message: 'The task has been successfully deleted',
-      data: result,
-      error: false,
-    });
   } catch (error) {
-    return res.status(400).json({
-      message: error,
+    res.status(500)({
+      message: 'Internal server error',
       data: undefined,
       error: true,
     });
@@ -58,6 +43,6 @@ const deleteTask = async (req, res) => {
 };
 
 export default {
-  createTask,
-  deleteTask,
+  getAllTasks,
+  getTasksById,
 };
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import tasksControllers from '../controllers/tasks';
-import tasksValidations from '../validations/tasks';
 
 const router = express.Router();
 
-router.post('/', tasksValidations.validateCreation, tasksControllers.createTask);
-router.delete('/:id', tasksControllers.deleteTask);
+router.get('/', tasksControllers.getAllTasks);
+router.get('/:id', tasksControllers.getTasksById);
 
 export default router;
