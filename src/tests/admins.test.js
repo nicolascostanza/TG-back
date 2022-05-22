47e244c0b5a49974d4321c04ca7af37768ad6b97
maria higinia medica <higiniamc@gmail.com>
2022-05-22 20:47:49 -0300

    TG-46: Test GET by ID

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest';
+import app from '../app';
 import Admins from '../models/Admins';
 import adminsSeed from '../seeds/admins';
-import app from '../app';
 
 beforeAll(async () => {
   await Admins.collection.insertMany(adminsSeed);
@@ -9,8 +9,37 @@ beforeAll(async () => {
 
 describe('GET by ID admins', () => {
   test('response should return a 200 status', async () => {
-    const response = await (await request(app).get('/admins')).send();
-    // eslint-disable-next-line no-console
-    console.log(response);
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return a false error', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('response should return an error, empty admin', async () => {
+    const response = await request(app).get('/admins/').send();
+    expect(response.error).toBe(true);
+  });
+
+  test('response should return an error, bad path', async () => {
+    const response = await request(app).get('/asdasd').send();
+    expect(response.error).toBe(true);
+  });
+
+  test('response should return an error', async () => {
+    const response = await request(app).get('/asdasd').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('response should return an ID', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.data.length).toBe(1);
+  });
+
+  test('response should return an ID', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.message).toEqual('The admin is:');
   });
 });
