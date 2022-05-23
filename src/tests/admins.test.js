a470b37b68899bea15f97109c56ac6294384bc99
maria higinia medica <higiniamc@gmail.com>
2022-05-23 00:02:33 -0300

    TG-46: mongoDB issues

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
   await Admins.collection.insertMany(adminsSeed);
 });
 
-describe('GET by ID admins', () => {
+describe('GET by ID /admins', () => {
   test('response should return a 200 status', async () => {
     const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
     expect(response.status).toBe(200);
@@ -20,26 +20,81 @@ describe('GET by ID admins', () => {
 
   test('response should return an error, empty admin', async () => {
     const response = await request(app).get('/admins/').send();
-    expect(response.error).toBe(true);
+    expect(response.status).toBe(404);
   });
 
   test('response should return an error, bad path', async () => {
     const response = await request(app).get('/asdasd').send();
-    expect(response.error).toBe(true);
-  });
-
-  test('response should return an error', async () => {
-    const response = await request(app).get('/asdasd').send();
     expect(response.status).toBe(404);
   });
 
-  test('response should return an ID', async () => {
+  test('response should return an admin with first name', async () => {
     const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
-    expect(response.body.data.length).toBe(1);
+    expect(response.body.data).toHaveProperty('firstName');
   });
 
-  test('response should return an ID', async () => {
+  test('response should return an admin with last name', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.data).toHaveProperty('lastName');
+  });
+
+  test('response should return an admin with email', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.data).toHaveProperty('email');
+  });
+
+  test('response should return an admin with password', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.data).toHaveProperty('password');
+  });
+
+  test('response should return an admin with active', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.data).toHaveProperty('active');
+  });
+
+  test('response should return an admin object', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const expected = {
+      _id: '60d4a32f257e066e9495ce12',
+      firstName: 'Alfonso',
+      lastName: 'Dalix',
+      email: 'alfonso@gmail.com',
+      password: '123456789',
+      active: 'true',
+    };
+    expect(response.body.data).toEqual(expect.objectContaining(expected));
+  });
+
+  test('response should return a successful message', async () => {
     const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
     expect(response.body.message).toEqual('The admin is:');
+  });
+});
+
+describe('DELETE /admins', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).delete('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return a false error', async () => {
+    const response = await request(app).delete('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('response should return an error, empty admin', async () => {
+    const response = await request(app).delete('/admins/').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('response should return an error, bad path', async () => {
+    const response = await request(app).delete('/asdasd').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('response should return a successful message', async () => {
+    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    expect(response.body.message).toEqual('Admin succesfully deleted');
   });
 });
