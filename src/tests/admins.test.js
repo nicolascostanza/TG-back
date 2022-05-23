5a95e3dd0a3ae316441e928212146f4e6aafc72b
maria higinia medica <higiniamc@gmail.com>
2022-05-23 00:59:38 -0300

    TG-46: Add delete

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -7,14 +7,16 @@ beforeAll(async () => {
   await Admins.collection.insertMany(adminsSeed);
 });
 
+const adminId = '60d4a32f257e066e9495ce12';
+
 describe('GET by ID /admins', () => {
   test('response should return a 200 status', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.status).toBe(200);
   });
 
   test('response should return a false error', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.error).toBe(false);
   });
 
@@ -29,32 +31,32 @@ describe('GET by ID /admins', () => {
   });
 
   test('response should return an admin with first name', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.data).toHaveProperty('firstName');
   });
 
   test('response should return an admin with last name', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.data).toHaveProperty('lastName');
   });
 
   test('response should return an admin with email', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.data).toHaveProperty('email');
   });
 
   test('response should return an admin with password', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.data).toHaveProperty('password');
   });
 
   test('response should return an admin with active', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.data).toHaveProperty('active');
   });
 
   test('response should return an admin object', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     const expected = {
       _id: '60d4a32f257e066e9495ce12',
       firstName: 'Alfonso',
@@ -67,20 +69,20 @@ describe('GET by ID /admins', () => {
   });
 
   test('response should return a successful message', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).get(`/admins/${adminId}`).send();
     expect(response.body.message).toEqual('The admin is:');
   });
 });
 
 describe('DELETE /admins', () => {
-  test('response should return a 200 status', async () => {
-    const response = await request(app).delete('/admins/60d4a32f257e066e9495ce12').send();
+  test('response should return a 204 status', async () => {
+    const response = await request(app).delete(`/admins/${adminId}`).send();
     expect(response.status).toBe(200);
   });
 
   test('response should return a false error', async () => {
-    const response = await request(app).delete('/admins/60d4a32f257e066e9495ce12').send();
-    expect(response.error).toBe(false);
+    const response = await request(app).delete(`/admins/${adminId}`).send();
+    expect(response.body.error).toBe(false);
   });
 
   test('response should return an error, empty admin', async () => {
@@ -94,7 +96,7 @@ describe('DELETE /admins', () => {
   });
 
   test('response should return a successful message', async () => {
-    const response = await request(app).get('/admins/60d4a32f257e066e9495ce12').send();
+    const response = await request(app).delete(`/admins/${adminId}`).send();
     expect(response.body.message).toEqual('Admin succesfully deleted');
   });
 });
