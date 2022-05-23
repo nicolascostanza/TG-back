6853c46b9582163dd1971cd0e2f830a15bc94339
SebastianCocci <sebacocci@gmail.com>
2022-05-23 17:16:49 -0300

    TG-43 created new branch

---
+++ src/tests/employee.test.js
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import app from '../app';
+import Employees from '../models/Employees';
+import employeesSeeds from '../seeds/employees-seed';
+
+beforeAll(async () => {
+  await Employees.collection.insertMany(employeesSeeds);
+});
+
+describe('GetById /employees', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should not return error', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('response should return error', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e84951234').send();
+    expect(response.error).toBe(true);
+  });
+
+  test('response should return error', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
+    expect(response.body.data.length).toBeGraterThan(0);
+  });
+});
+
+/* describe('Delete /employees', () => {
+  test('It should delete a employee', async () => {
+    const response = await request(app).delete('/employees/60d4a32f257e066e8495ce12');
+    expect(response.status).toBe(200);
+  });
+
+  test('Delete should return error', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('Delete should return error', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e84951234').send();
+    expect(response.error).toBe(true);
+  });
+}); */
