3839d681b59de12e1681dddd5a98c6760f46a5e6
SebastianCocci <sebacocci@gmail.com>
2022-05-24 10:06:23 -0300

    TG-43: finish the get by id and delete employee test

--- src/tests/employee.test.js
+++ src/tests/employee.test.js
@@ -18,24 +18,29 @@ describe('GetById /employees', () => {
     expect(response.error).toBe(false);
   });
 
-  test('response should return error', async () => {
+  test('the employee with id X much was not found', async () => {
     const response = await request(app).get('/employees/60d4a32f257e066e84951234').send();
     expect(response.status).toBe(400);
   });
 
-  test('response should return error', async () => {
+  test('returns the required information', async () => {
     const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
     expect(response.body.data).not.toBeNull();
+  });
+
+  test('response should return an admin with active', async () => {
+    const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
+    expect(response.body.data).toHaveProperty('email');
   });
 });
 
 describe('Delete /employees', () => {
   test('Delete should return error', async () => {
     const response = await request(app).get('/employees/60d4a32f257e066e8495fa15').send();
-    expect(response.error).toBe(true);
+    expect(response.body.error).toBe(true);
   });
 
-  test('Delete should return error', async () => {
+  test('Delete should not return error', async () => {
     const response = await request(app).get('/employees/60d4a32f257e066e8495ce12').send();
     expect(response.error).toBe(false);
   });
@@ -43,5 +48,7 @@ describe('Delete /employees', () => {
   test('It should delete a employee', async () => {
     const response = await request(app).delete('/employees/60d4a32f257e066e8495ce12');
     expect(response.status).toBe(200);
+    expect(response.body.msg).toEqual('The employee with former id of 60d4a32f257e066e8495ce12 has been succesfully deleted');
+    console.log(response.body.msg);
   });
 });
