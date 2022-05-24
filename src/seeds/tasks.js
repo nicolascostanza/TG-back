9ff06ecce53c55534c98982358cc75831e3c229d
Javier Cervera <javieralejandrocervera@gmail.com>
2022-05-24 00:03:05 -0300

    TG-45: fix variables

--- src/seeds/tasks.js
+++ src/seeds/tasks.js
@@ -4,6 +4,7 @@ export default [{
   _id: mongoose.Types.ObjectId('60a4a32f247e066e9495ce12'),
   parentProject: '68a4a32f247e066e9495ce12',
   taskName: 'Taks',
+  taskDescription: 'Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description',
   assignedEmployee: ['60d4a32f257e066e8495ce12'],
   startDate: '20/05/2022',
   status: 'Ready to deliver',
