fd018c5ed45040bc3f5aa5fb8d0d698c7dd277d6
Nancy Cima <nancycimabertoni@gmail.com>
2022-05-24 21:01:00 -0300

    TG-57: added PUT test

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -339,6 +339,316 @@ describe('POST /time-sheets', () => {
   });
 });
 
+describe('PUT /time-sheets', () => {
+  test('It should update the timesheet and the status should be 200', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test('It should be return false error', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.body.error).toBeFalsy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing employeeId. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: '',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing the project. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: '',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing the date. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing the hours. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: '',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing the approved status. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: '',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update a timesheet: the request is missing required field. The status should be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: '',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update a timesheet: the request is missing the role. ClientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: '',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('Route does not exist. The status should be 404', async () => {
+    const response = await request(app).post('/xvxvxm').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(404);
+  });
+
+  test('Route does not exist. clientError has to be true', async () => {
+    const response = await request(app).post('/xvxvxm').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update the timesheet because he description is too short, status should to be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Lo',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update the timesheet because he description is too short, clientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Lo',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update the timesheet because he description is too long, status should to be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Lorem impsum lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update the timesheet because he description is too long, status has to be 400, clientError should to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Lorem impsum lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description, Lorem impsum tuki tuki lorem ipsum tuki tuki this is a description',
+      project: 'project a ',
+      date: '05/24/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+
+  test('It cannot update the timesheet because the date format is invalid, status should to be 400', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '24/25/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('It cannot update the timesheet because the date format is invalid, clientError has to be true', async () => {
+    const response = await request(app).put('/time-sheets/62832da494417525e9b7b0c2').send({
+      employeeId: '60d4a32f257e066e8495ce12',
+      description: 'Hi, i am a descrption',
+      project: 'project a ',
+      date: '24/25/2022',
+      hours: '50',
+      task: '60a4a32f247e066e9495ce12',
+      approved: 'true',
+      role: 'DEV',
+    });
+    expect(response.clientError).toBeTruthy();
+  });
+});
+
 describe('GET /time-sheets', () => {
   test('response should return a 200 status', async () => {
     const response = await request(app).get('/time-sheets').send();
