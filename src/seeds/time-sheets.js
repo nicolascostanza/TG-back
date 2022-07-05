9db9c66ac32f9a8419146ac40733a996fcb50da9
martinalbrnz <martinalbor96@gmail.com>
2022-07-05 00:06:44 -0300

    TG-150: Test basic update

--- src/seeds/time-sheets.js
+++ src/seeds/time-sheets.js
@@ -3,11 +3,9 @@ import mongoose from 'mongoose';
 export default [{
   _id: mongoose.Types.ObjectId('62832da494417525e9b7b0c2'),
   employeeId: '60d4a32f257e066e8495ce12',
-  description: 'Hi, i am a descrption',
-  project: 'project a ',
+  projectId: '68a4a32f247e066e9495ce12',
   date: '05/20/2022',
   hours: '45',
-  task: ['60a4a32f247e066e9495ce12'],
+  task: '60a4a32f247e066e9495ce12',
   approved: 'true',
-  role: 'DEV',
 }];
